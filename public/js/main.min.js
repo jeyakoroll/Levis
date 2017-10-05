9e2f03f387d94302c49d46f909631135f6a319bb
Jenya Korolenko <jeyakoroll@gmail.com>
2017-10-05 16:58:41 +0200

    scroll top

--- public/js/main.min.js
+++ public/js/main.min.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
 	
 
+	// switching images in models area
+
+    $( '.gallery__preview-item' ).on('click', slideHandler );
+    $( '.gallery__preview-item' ).bind( "tap", slideHandler );
+
+    function slideHandler (e) {
+        let target = $(e.target);
+        if (target.hasClass('gallery__preview-item')) {
+            let img = target.closest('.gallery__preview-show').find('.gallery__preview-pic');
+            let src = target.data('src');
+            img.attr('src', src);
+            img.hide();
+            img.fadeIn();
+        }
+    }
+	
 	// output of the description in the catalog
 	const buy = '<button class="gallery__descr-button_buy">Купить</button>',
 				descr = '<button class="gallery__descr-button_size">Узнать свой размер</button>',
@@ -31,8 +47,6 @@ $(document).ready(function () {
 				}
 
 
-
-
 	// output for desktop
 	
 
@@ -49,4 +63,109 @@ $(document).ready(function () {
     $('.footer__form-text input[name=name_first]').attr("placeholder","Имя");
     $('.footer__form-text input[name=name_last]').attr("placeholder","Телефон");
 
+
+
+		// go to the menu area
+    // about area
+    $('.header__link-about').on('click', showAboutWatch);
+    $('.header__link-about').bind('tap', showAboutWatch);
+
+    function showAboutWatch (e) {
+        e.preventDefault();
+        showAbout($(this).attr('href'), true);
+    }    
+
+    // about area
+    $('.header__link-gallery').on('click', showGalleryWatch);
+    $('.header__link-gallery').bind('tap', showGalleryWatch);
+
+    function showGalleryWatch (e) {
+        e.preventDefault();
+        showGallery($(this).attr('href'), true);
+    }    
+
+  // advantages area
+    $('.header__link-advantages').on('click', showAdvantagesWatch);
+    $('.header__link-advantages').bind('tap', showAdvantagesWatch);
+
+    function showAdvantagesWatch (e) {
+        e.preventDefault();
+        showAdvantages($(this).attr('href'), true);
+    }    
+
+    // buy area
+    $('.header__link-buy').on('click', showBuyWatch);
+    $('.header__link-buy').bind('tap', showBuyWatch);
+
+    function showBuyWatch (e) {
+        e.preventDefault();
+        showBuy($(this).attr('href'), true);
+    }    
+
+    // reviews area
+    $('.header__link-reviews').on('click', showReviewsWatch);
+    $('.header__link-reviews').bind('tap', showReviewsWatch);
+
+    function showReviewsWatch (e) {
+        e.preventDefault();
+        showReviews($(this).attr('href'), true);
+    }
+
+    showReviews(window.location.hash, false);
+
 });
+
+    function showAbout(section, isAnimate) {
+        let 
+            direction = section.replace(/#/, ''),
+            reqSection = $('.history>.row').filter('[data-section="' + direction +'"]'),
+            reqSectionPos = reqSection.offset().top;
+
+        if(isAnimate) {
+            $('body, html').animate({scrollTop: reqSectionPos}, 500);
+        }
+    }
+
+    function showGallery(section, isAnimate) {
+        let 
+            direction = section.replace(/#/, ''),
+            reqSection = $('.gallery__title').filter('[data-section="' + direction +'"]'),
+            reqSectionPos = reqSection.offset().top;
+
+        if(isAnimate) {
+            $('body, html').animate({scrollTop: reqSectionPos}, 500);
+        }
+    }
+
+    function showAdvantages(section, isAnimate) {
+        let 
+            direction = section.replace(/#/, ''),
+            reqSection = $('.advantages__title').filter('[data-section="' + direction +'"]'),
+            reqSectionPos = reqSection.offset().top;
+
+        if(isAnimate) {
+            $('body, html').animate({scrollTop: reqSectionPos}, 500);
+        }
+    }
+
+    function showBuy(section, isAnimate) {
+        let 
+            direction = section.replace(/#/, ''),
+            reqSection = $('.relationship').filter('[data-section="' + direction +'"]'),
+            reqSectionPos = reqSection.offset().top;
+
+        if(isAnimate) {
+            $('body, html').animate({scrollTop: reqSectionPos}, 500);
+        }
+    }
+
+    function showReviews(section, isAnimate) {
+        let 
+            direction = section.replace(/#/, ''),
+            reqSection = $('.reviews__title').filter('[data-section="' + direction +'"]'),
+            reqSectionPos = reqSection.offset().top;
+
+        if(isAnimate) {
+            $('body, html').animate({scrollTop: reqSectionPos}, 500);
+        }
+    }
